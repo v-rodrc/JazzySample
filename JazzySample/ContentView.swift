2fd61b183b76a71c7b5b20f3453549fa7c1a984d
Christopher Rodriguez <christopher.rodriguez@softpository.com>
2020-04-18 19:34:52 -0500

    Initial Commit

---
+++ JazzySample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JazzySample
+//
+//  Created by Christopher Rodriguez on 4/18/20.
+//  Copyright © 2020 Christopher Rodriguez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
